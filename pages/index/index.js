b54709cf6b973cb876295931eb44f4d77e47ed5a
che mei jie <chemeijie@qq.com>
2019-04-10 17:33:14 +0800

    first commit

---
+++ pages/index/index.js
@@ -0,0 +1,65 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    itemBody_1:true,
+    itemBody_2: true,
+    itemBody_3: true,
+  },
+
+  //切换item显示
+  toggleItem:function(e){
+    var id = e.target.dataset.id;
+
+    var that = this;
+
+    this.allItemClose();
+
+    if(id == 1){
+      if (that.data.itemBody_1 == true) {
+        that.setData({ itemBody_1: false })
+      } else {
+        that.setData({ itemBody_1: true })
+      }
+    }else if(id == 2){
+      if (that.data.itemBody_2 == true) {
+        that.setData({ itemBody_2: false })
+      } else {
+        that.setData({ itemBody_2: true })
+      }
+    }else if (id == 3) {
+      if (that.data.itemBody_3 == true) {
+        that.setData({ itemBody_3: false })
+      } else {
+        that.setData({ itemBody_3: true })
+      }
+    }
+  
+  },
+  //重置所有item
+  allItemClose:function(){
+    this.setData({
+      itemBody_1: true,
+      itemBody_2: true,
+      itemBody_3: true,
+    })
+  },
+  //跳转列表页
+  goList:function(e){
+
+    const id = e.target.dataset.id;
+
+    const url = "/pages/list/list?id=" + id;//得到页面url 
+
+    wx.navigateTo({
+
+      url: url
+
+    })
+
+  }
+
+
+})
