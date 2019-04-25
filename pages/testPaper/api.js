cab08f87c75f85a59b3442328bc8cfac8c363db7
vagrant <vagrant@homestead>
2019-04-25 15:29:28 +0000

    临时提交

---
+++ pages/testPaper/api.js
@@ -0,0 +1,27 @@
+const app = getApp()
+
+const api = {
+  /**
+   * 获取题目详细信息
+   */
+  getPoolDetail: function (data) {
+    let url = '/pools/show/' + data.pool_id;
+    let header = {};
+
+    return app.fetch(url, {}, 'GET', header);
+  },
+  toggleStatus: function (data) {
+    let url = '/pools/status';
+    let header = {};
+
+    return app.fetch(url, data, 'PUT', header);
+  },
+  getNextOrLast: function (data) {
+    let url = '/pools/get_next_or_last/' + data.type + '/' + data.sn + '/' + data.tabType;
+    let header = {};
+
+    return app.fetch(url, {}, 'GET', header);
+  }
+}
+
+module.exports = api;
--- pages/testPaper/testPaper.js
+++ pages/testPaper/testPaper.js
@@ -1,18 +1,40 @@
 // pages/testPaper/testPaper.js
+
+const app = getApp();
+
+const api = require("./api.js");
+
+const WxParse = require('../../plugins/wxParse/wxParse.js');
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    answersHidden: false,
+    detail: null,
+    totalScore:10,
+    score:0,
+    tabType:'all',
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var that = this;
+    //获取类目信息
+    api.getPoolDetail({ pool_id: 1 }).then(data => {
+      this.setData({
+        detail: data.data[0],
+      })
+      //富文本框
+      var answers = that.data.detail.answers;
+      WxParse.wxParse('detail_answers', 'html', answers, that, 5);
+    }).catch(error => {
+      console.log(error);
+    });
   },
 
   /**
@@ -47,7 +69,7 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    wx.stopPullDownRefresh();
   },
 
   /**
@@ -62,5 +84,66 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  /**
+   * 打分
+   */
+  grade:function(e){
+    var score = e.currentTarget.dataset.score;
+    this.setData({
+      score: score,
+    })
+
+  },
+    //切换答案显示状态
+  toggleAnswers: function () {
+    if (this.data.answersHidden) {
+      this.setData({
+        answersHidden: false
+      })
+    } else {
+      this.setData({
+        answersHidden: true
+      })
+    }
+  },
+  //切换已背 未背状态
+  toggleRead: function () {
+    var that = this
+    var detail_status = 'detail.status';
+    api.toggleStatus({ pool_id: this.data.detail.id, status: this.data.detail.status }).then(data => {
+      wx.showToast({
+        title: that.data.detail.status == 1 ? '标记为未背' : '标记为已背',
+        icon: 'success',
+        duration: 3000
+      });
+
+      that.setData({
+        [detail_status]: that.data.detail.status == 1 ? -1 : 1,
+      })
+    }).catch(error => {
+      console.log(error);
+    });
+  },
+  //下一个上一个
+  nextLastQuestion: function (e) {
+    var that = this;
+    api.getNextOrLast({
+      type: e.currentTarget.dataset.type,
+      sn: this.data.detail.sn,
+      tabType: this.data.tabType
+    }).then(data => {
+      if (data.data.length == 0) {
+        app.toast('已经到头了');
+      } else {
+        that.setData({
+          detail: data.data[0],
+          // answersHidden:true,
+        })
+        //富文本框
+        var answers = that.data.detail.answers;
+        WxParse.wxParse('detail_answers', 'html', answers, that, 5);
+      }
+    })
   }
 })
